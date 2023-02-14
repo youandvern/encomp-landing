29ef16f76eba73544c1d8a3aa1fbaf584422d23f
youandvern <ayounguw@gmail.com>
2023-02-13 21:55:28 -0800

    add pre-rendering

--- encomp-landing/src/index.tsx
+++ encomp-landing/src/index.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { hydrate, render } from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+if (rootElement?.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
+// const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
